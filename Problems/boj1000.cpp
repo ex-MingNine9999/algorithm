d73be9ba1c14dced82358a15f7c0a4346faf5915
SONGMINGU <alsrnx@gmail.com>
2020-04-02 00:23:05 +0900

    Solutions

---
+++ Problems/boj1000.cpp
@@ -0,0 +1,19 @@
+//Problem Number : 1000
+//Problem Title : None
+//Problem Link : https://www.acmicpc.net/problem/1000
+
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+	
+	scanf("%d%d", &a, &b);
+
+	printf("%d", a + b);
+
+	return 0;
+}
