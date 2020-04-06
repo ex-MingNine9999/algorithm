a72c57c61280c8c51ac2248ab2e1f2de2242dc75
MingNine9999 <mingnine9@soongsil.ac.kr>
2020-04-07 03:19:16 +0900

    Solutions

---
+++ Problems/boj11561.cpp
@@ -0,0 +1,45 @@
+//Problem Number : 11561
+//Problem Title : None
+//Problem Link : https://www.acmicpc.net/problem/11561
+
+#include <iostream>
+#include <algorithm>
+
+using ll = long long;
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int TC;
+
+	cin >> TC;
+
+	while (TC--) {
+		ll n;
+		ll ans = 0;
+		ll jmp = 1;
+
+		cin >> n;
+
+		int l = 1, r = 1e9;
+		while (l < r) {
+			ll mid = (l + r + 1) / 2;
+			ll ta = (mid + 1) * mid / 2;
+
+			if (ta <= n) {
+				l = mid;
+			}
+			else {
+				r = mid - 1;
+			}
+		}
+
+		cout << l << '\n';
+	}
+
+
+	return 0;
+}
