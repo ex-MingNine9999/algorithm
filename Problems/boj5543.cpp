d589a5832b00e411e842362978a3be9c11ae9874
MingNine9999 <mingnine9@soongsil.ac.kr>
2020-04-13 20:08:00 +0900

    Solutions

---
+++ Problems/boj5543.cpp
@@ -0,0 +1,31 @@
+//Problem Number : 5543
+//Problem Title : None
+//Problem Link : https://www.acmicpc.net/problem/5543
+
+#include <iostream>
+#include <algorithm>
+
+#define INF 1234567890
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a = INF, b = INF;
+	int in;
+	for (int i = 0; i < 3; i++) {
+		cin >> in;
+		a = min(a, in);
+	}
+	for (int i = 0; i < 2; i++) {
+		cin >> in;
+		b = min(b, in);
+	}
+
+	cout << a + b - 50;
+
+	return 0;
+}
