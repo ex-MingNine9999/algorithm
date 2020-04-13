a4651f79d20ae15f3af38c0e1d2ef8ae28cbc622
MingNine9999 <mingnine9@soongsil.ac.kr>
2020-04-13 20:16:05 +0900

    Solutions

---
+++ Problems/boj2921.cpp
@@ -0,0 +1,29 @@
+//Problem Number : 2921
+//Problem Title : None
+//Problem Link : https://www.acmicpc.net/problem/2921
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1111];
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		dp[i] = i * (i + 1);
+		dp[i] += dp[i] / 2;
+		dp[i] += dp[i - 1];
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
