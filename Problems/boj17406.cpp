9868d454b775b850c69c0af084bde769de340cb1
MingNine9999 <mingnine9@soongsil.ac.kr>
2020-04-22 23:13:59 +0900

    Solutions

--- Problems/boj17406.cpp
+++ Problems/boj17406.cpp
@@ -11,7 +11,6 @@ using namespace std;
 
 int n, m, k;
 int a[55][55];
-int tmp[55][55];
 int rcs[11][3];
 int check[11];
 int tt[6];
@@ -19,8 +18,14 @@ int tt[6];
 const int goY[] = { 1,0,-1,0 };
 const int goX[] = { 0,1,0,-1 };
 
-void rotate(int r, int c, int s)
+void rotation(int r, int c, int s, int arr[55][55], int tmp[55][55])
 {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			tmp[i][j] = arr[i][j];
+		}
+	}
+
 	for (int i = 1; i <= s; i++) {
 		int y = r - i;
 		int x = c - i;
@@ -42,25 +47,14 @@ void rotate(int r, int c, int s)
 	}
 }
 
-int calcul(void)
+int calcul(int arr[55][55])
 {
 	int ret = INF;
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			tmp[i][j] = a[i][j];
-		}
-	}
-
-	for (int i = 0; i < k; i++) {
-		int order = tt[i];
-		rotate(rcs[order][0], rcs[order][1], rcs[order][2]);
-	}
 	
 	for (int i = 1; i <= n; i++) {
 		int t = 0;
 		for (int j = 1; j <= m; j++) {
-			t += tmp[i][j];
+			t += arr[i][j];
 		}
 		ret = min(ret, t);
 	}
@@ -68,18 +62,21 @@ int calcul(void)
 	return ret;
 }
 
-int findMin(int idx)
+int findMin(int idx, int arr[55][55])
 {
 	int ret = INF;
+	int tmp[55][55];
+
 	if (idx == k) {
-		return calcul();
+		return calcul(arr);
 	}
 
 	for (int i = 0; i < k; i++) {
 		if (check[i]) continue;
 		check[i] = 1;
 		tt[idx] = i;
-		ret = min(ret, findMin(idx + 1));
+		rotation(rcs[i][0], rcs[i][1], rcs[i][2], arr, tmp);
+		ret = min(ret, findMin(idx + 1, tmp));
 		check[i] = 0;
 	}
 
@@ -97,14 +94,14 @@ int main(void)
 			cin >> a[i][j];
 		}
 	}
-	
+
 	for (int i = 0; i < k; i++) {
 		for (int j = 0; j < 3; j++) {
 			cin >> rcs[i][j];
 		}
 	}
 
-	cout << findMin(0);
+	cout << findMin(0, a);
 
 	return 0;
 }
