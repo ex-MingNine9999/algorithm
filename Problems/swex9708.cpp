b7a588822c4b6a57b8636e734cda9c713670bb79
MingNine9999 <mingnine9@soongsil.ac.kr>
2020-05-05 16:23:40 +0900

    solution

---
+++ Problems/swex9708.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[111111];
+int N[1111111];
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int TC;
+
+	cin >> TC;
+
+	for (int tc = 1; tc <= TC; tc++) {
+		int n;
+		int MAX = 0;
+		int ans = 0;
+
+		fill(N, N + 1111111, 0);
+
+		cin >> n;
+
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+			N[a[i]] = 1;
+
+			MAX = max(MAX, a[i]);
+		}
+
+		sort(a, a + n);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 2; a[i] * j <= MAX; j++) {
+				if (N[a[i] * j]) {
+					N[a[i] * j] = max(N[a[i] * j], N[a[i]] + 1);
+				}
+			}
+
+			ans = max(ans, N[a[i]]);
+		}
+		
+
+		cout << '#' << tc << ' ' << ans << '\n';
+	}
+
+	return 0;
+}
