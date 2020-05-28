3484dc0abdb04b5f10d8682a6a39e2383de6fe30
MingNine9999 <mingnine9@soongsil.ac.kr>
2020-05-28 18:20:46 +0900

    Solutions

---
+++ Problems/boj17252.cpp
@@ -0,0 +1,35 @@
+//Problem Number : 17252
+//Problem Title : »ï»ïÇÑ ¼ö
+//Problem Link : https://www.acmicpc.net/problem/17252
+
+#include <iostream>
+#include <algorithm>
+
+using ll = long long;
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	bool ans = true;
+
+	cin >> n;
+
+	if (n == 0) {
+		ans = false;
+	}
+	while (n != 0 && ans) {
+		if (n % 3 == 2) {
+			ans = false;
+			break;
+		}
+		n /= 3;
+	}
+
+	cout << (ans ? "YES" : "NO");
+
+	return 0;
+}
